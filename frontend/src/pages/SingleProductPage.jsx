891fe7f79b18e28e7e6c5b95b7366ce1c1246ee2
Shadab Khan <mdshadab21july@gmail.com>
2024-09-09 09:44:19 +0530

    finish single product page

--- frontend/src/pages/SingleProductPage.jsx
+++ frontend/src/pages/SingleProductPage.jsx
@@ -35,16 +35,53 @@ const SingleProductPage = () => {
         <div className="header flex justify-between items-center p-4 bg-white">
           <h1 className="text-3xl font-bold">Peter Miles</h1>
         </div>
+
         {productDetails ? (
-          <div className="product-details p-4">
-            <h2 className="text-2xl font-semibold">{productDetails.name}</h2>
-            <img
-              src={productDetails.imgURL}
-              alt={productDetails.name}
-              className="w-full h-auto"
-            />
-            <p className="mt-2">{productDetails.description}</p>
-            <p className="mt-2 text-xl font-bold">${productDetails.price}</p>
+          <div className="product-details px-8 py-20 flex justify-between items-center mr-10 w-full border-0 border-yellow-500">
+            <div className="flex-1 flex items-center justify-center">
+              <img
+                src={productDetails.imgURL}
+                alt={productDetails.name}
+                className="object-cover h-[640px]"
+                loading="lazy"
+              />
+            </div>
+            <div className="flex flex-col self-start border-0 border-red-500 w-full h-full ml-10 flex-1 p-8">
+              <h2 className="text-4xl font-thin">{productDetails.name}</h2>
+              <p className="mt-6 max-w-[70%]">
+                {/* {productDetails.description} */}
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aliquam deleniti explicabo voluptatum magnam adipisci quae,
+                excepturi aut eligendi pariatur expedita, natus id cumque
+                officia nihil asperiores hic, commodi dolor aperiam laboriosam
+                laudantium quaerat! Autem distinctio modi minima et nostrum
+                officiis?
+              </p>
+              <p className="mt-12 text-2xl font-base">
+                ${productDetails.price}
+              </p>
+
+              <div className="mt-16">
+                <div className="mb-12">
+                  <span>Color:</span>
+                  <select
+                    name="colors"
+                    id="colorSelection"
+                    className="capitalize border p-2 rounded-md ml-2 mr-2"
+                  >
+                    {productDetails.color.map((colItem) => (
+                      <option name={colItem} key={productDetails.color.length}>
+                        {colItem}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+
+                <button className="border py-2 px-5 rounded bg-black text-white font-medium cursor-pointer w-[12rem]">
+                  Add to cart
+                </button>
+              </div>
+            </div>
           </div>
         ) : (
           <p>No product details available.</p>
